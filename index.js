cfdeeb142a13b2079e9f46e799e4b597b8b69e00
Shivam_Mudaliar <mathivanan.shivam@gmail.com>
2022-07-11 17:10:09 +0530

    Final Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function handleselect(elm){
+    window.location=elm.value+'.php';
+}
+function github(){
+    window.open('https://github.com/shiv-1203','_blank');
+}
+function linkedin(){
+    window.open('https://www.linkedin.com/in/shivam-mudaliar-145a58201/','_blank');
+}
